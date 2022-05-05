fc26f373baaacf8ea8668e7db88e4dac2cabec4b
saucefoong <sheeyao@hotmail.com>
2022-05-06 07:58:41 +0800

    First Commit

---
+++ pages/login.tsx
@@ -0,0 +1,54 @@
+import React, { SyntheticEvent, useState } from "react";
+import Layout from "../layouts/Layout";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userName: username,
+        password,
+      }),
+    });
+
+    await router.push("/");
+  };
+
+  return (
+    <Layout>
+      <form onSubmit={submit}>
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          required
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+      </form>
+    </Layout>
+  );
+};
+
+export default Login;
