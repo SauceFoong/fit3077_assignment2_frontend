4192454d10f4f1a13976de58c38025cf9dd1eb96
wengsheng <whee0017@student.monash.edu>
2022-05-27 21:55:24 +0800

    added assignment 3 frontend

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/active-booking":["static\u002Fchunks\u002Fpages\u002Factive-booking.js"],sortedPages:["\u002F_app","\u002F_error","\u002Factive-booking"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/admin-edit":["static\u002Fchunks\u002Fpages\u002Fadmin-edit.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fadmin-edit"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
